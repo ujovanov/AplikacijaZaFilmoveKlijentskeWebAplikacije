34803dc4a8b8c287c35ee589f3db8530d154fe7e
Uros Jovanov <ujovanov@gmail.com>
2025-03-07 11:42:07 +0100

    adding single movie page, adding empty cart page and adding another loader

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
+import { Movie } from '../../models/movie.model';
+import { NgIf, NgFor, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
-  imports: [],
+  imports: [NgIf, NgFor, DatePipe],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit {
+  movie?: Movie;
+  loading = true;
+  error = false;
 
+  constructor(private route: ActivatedRoute) {}
+
+  async ngOnInit() {
+    try {
+      const shortUrl = this.route.snapshot.params['shortUrl'];
+      const response = await axios.get<Movie>(`https://movie.pequla.com/api/movie/short/${shortUrl}`);
+      this.movie = response.data;
+    } catch (error) {
+      console.error('Error fetching movie:', error);
+      this.error = true;
+    } finally {
+      this.loading = false;
+    }
+  }
 }
